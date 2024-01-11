c679308a99f03ec09842884a228caa82f07539e2
BouaziziMahdi <mahdibouazizi10@gmail.com>
2024-01-11 15:21:42 +0100

    "add repositories"

---
+++ src/main/java/com/portilo/portilo/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.portilo.portilo.repository;
+
+import com.portilo.portilo.Entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/portilo/portilo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.portilo.portilo.repository;
+
+import com.portilo.portilo.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username);
+
+}
