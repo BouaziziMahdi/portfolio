7045494afb8260456a250a1b770b9d91e381331f
BouaziziMahdi <mahdibouazizi10@gmail.com>
2024-01-11 15:16:32 +0100

    "add entity"

--- src/main/java/com/portilo/portilo/Entity/User.java
+++ src/main/java/com/portilo/portilo/Entity/User.java
@@ -1,13 +1,17 @@
 package com.portilo.portilo.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,7 +19,7 @@ import lombok.Setter;
 @Setter
 @Table(name = "user")
 @Entity
-public class User {
+public class User implements UserDetails {
     @GeneratedValue
     @Id
     private Integer id ;
@@ -23,5 +27,44 @@ public class User {
     private String lastName ;
     private String email ;
     private String password ;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map( r -> new SimpleGrantedAuthority(r.getName()))
+                .collect( Collectors.toList());
+    }
 
+
+
+    public String getPasswword() {
+        return password;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
