864f61c197e07ad69757abbaf6d8b7ee28ea06b2
BouaziziMahdi <mahdibouazizi10@gmail.com>
2024-01-11 15:42:01 +0100

    "add mapper && services "

--- src/main/java/com/portilo/portilo/Entity/User.java
+++ src/main/java/com/portilo/portilo/Entity/User.java
@@ -1,10 +1,7 @@
 package com.portilo.portilo.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +14,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Table(name = "user")
 @Entity
 public class User implements UserDetails {
--- src/main/java/com/portilo/portilo/mapper/RegisterRequest.java
+++ src/main/java/com/portilo/portilo/mapper/RegisterRequest.java
@@ -1,15 +1,19 @@
 package com.portilo.portilo.mapper;
 
 import com.portilo.portilo.Entity.Role;
+import com.portilo.portilo.Entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class RegisterRequest {
 
     private String firstname;
@@ -17,4 +21,21 @@ public class RegisterRequest {
     private String email;
     private String password;
     private Role role;
+
+    public static User toEntity(RegisterRequest registerRequest) {
+        return User.builder()
+                .firstName (registerRequest.getFirstname())
+                .lastName(registerRequest.getLastname())
+                .email(registerRequest.getEmail())
+                .password(registerRequest.getPassword())
+                .build();
+    }
+    public RegisterRequest fromEntity(User user) {
+        return RegisterRequest.builder()
+                .firstname(user.getFirstName())
+                .lastname(user.getLastName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
 }
